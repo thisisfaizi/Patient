2c39a3b5dc746c888f13edb4266f2420cdf209b9
thisisfaizi <xheikhfazan3522@gmail.com>
2023-03-30 16:29:16 +0500

    patient

---
+++ src/patient/Patient2.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package patient;
+
+/**
+ *
+ * @author Master
+ */
+public class Patient2 {
+    private String name;
+
+    // Constructor with name parameter
+    public Patient2(String name) {
+        if ("".equals(name) ) {
+            this.name = "Unknown";
+        } else {
+            this.name = name;
+        }
+    }
+
+    // Method to print patient name
+    public void printName() {
+        System.out.println("Patient Name: " + name);
+    }
+public static void main(String[] args) {
+        // Create patient objects with and without name parameter
+        Patient patient1 = new Patient("John");
+        Patient patient2 = new Patient("");
+       
+
+        // Print patient names
+        patient1.printName();
+        patient2.printName();
+     
+    } 
+   
+}
+
+
+
+
+
